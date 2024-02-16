ee1475b27d9449a61642e0a354627773a1a3a73d
Brian Seidman <bseidman@hotmail.com>
2024-02-15 22:21:00 -0600

    wip: another lunr update

--- lunr/lunr-store.js
+++ lunr/lunr-store.js
@@ -3,47 +3,6 @@ layout: null
 ---
 
 var store = [
-  {%- for doc in docs -%}
-  {%- if doc.header.teaser -%}
-    {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-  {%- else -%}
-    {%- assign teaser = site.teaser -%}
-  {%- endif -%}
-  {%- if doc.title -%}
-  {
-    "title": {{ doc.title | jsonify }},
-    "excerpt":
-      {%- if site.search_full_content == true -%}
-        {{ doc.content | newline_to_br |
-          replace:"<br />", " " |
-          replace:"</p>", " " |
-          replace:"</h1>", " " |
-          replace:"</h2>", " " |
-          replace:"</h3>", " " |
-          replace:"</h4>", " " |
-          replace:"</h5>", " " |
-          replace:"</h6>", " "|
-        strip_html | strip_newlines | jsonify }},
-      {%- else -%}
-        {{ doc.content | newline_to_br |
-          replace:"<br />", " " |
-          replace:"</p>", " " |
-          replace:"</h1>", " " |
-          replace:"</h2>", " " |
-          replace:"</h3>", " " |
-          replace:"</h4>", " " |
-          replace:"</h5>", " " |
-          replace:"</h6>", " "|
-        strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-      {%- endif -%}
-    "categories": {{ doc.categories | jsonify }},
-    "tags": {{ doc.tags | jsonify }},
-    "url": {{ doc.url | relative_url | jsonify }},
-    "teaser": {{ teaser | relative_url | jsonify }}
-  },
-{%- endif -%}
-{%- endfor -%}
-
   {%- for c in site.collections -%}
     {%- if forloop.last -%}
       {%- assign l = true -%}
