927fb5c50bfb88e2ab41b542ef30f226eca89ab6
Brian Seidman <bseidman@hotmail.com>
2024-02-15 22:34:50 -0600

    wip: another lunr update

--- lunr/lunr-store.js
+++ lunr/lunr-store.js
@@ -1,8 +1,51 @@
 ---
-layout: null
+layout: none
 ---
 
+{%- assign docs = site.pages  -%}
+
 var store = [
+  {%- for doc in docs -%}
+    {%- if doc.header.teaser -%}
+      {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
+    {%- else -%}
+      {%- assign teaser = site.teaser -%}
+    {%- endif -%}
+    {%- if doc.title -%}
+    {
+      "title": {{ doc.title | jsonify }},
+      "excerpt":
+        {%- if site.search_full_content == true -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+        {%- endif -%}
+      "categories": {{ doc.categories | jsonify }},
+      "tags": {{ doc.tags | jsonify }},
+      "url": {{ doc.url | relative_url | jsonify }},
+      "teaser": {{ teaser | relative_url | jsonify }}
+    },
+  {%- endif -%}
+  {%- endfor -%}
+  
   {%- for c in site.collections -%}
     {%- if forloop.last -%}
       {%- assign l = true -%}
@@ -42,48 +85,43 @@ var store = [
           {%- endif -%}
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {%- if teaser contains "://" -%}
-            {{ teaser | jsonify }}
-          {%- else -%}
-            {{ teaser | absolute_url | jsonify }}
-          {%- endif -%}
+        "url": {{ doc.url | relative_url | jsonify }},
+        "teaser": {{ teaser | relative_url | jsonify }}
       }{%- unless forloop.last and l -%},{%- endunless -%}
     {%- endfor -%}
-  {%- endfor -%},
-  {%- for doc in site.pages -%}
-  {%- if doc.title and doc.title != "Publications" and doc.title != "People" and doc.title != "News Archive" -%}
+  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
+  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
+  {%- for doc in pages -%}
     {%- if forloop.last -%}
       {%- assign l = true -%}
     {%- endif -%}
-    {
-      "title": {{ doc.title | jsonify }},
-      "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "url": {{ doc.url | absolute_url | jsonify }}
-    }{%- unless forloop.last and l -%},{%- endunless -%}
-  {%- endif -%}
-  {%- endfor -%}]
+  {
+    "title": {{ doc.title | jsonify }},
+    "excerpt":
+        {%- if site.search_full_content == true -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+        {%- endif -%}
+      "url": {{ doc.url | absolute_url | jsonify }}
+  }{%- unless forloop.last and l -%},{%- endunless -%}
+  {%- endfor -%}
+{%- endif -%}]
